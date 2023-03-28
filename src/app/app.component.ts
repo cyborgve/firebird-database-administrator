d73c42c01aabef8654d42f78d587f2ff18f3a784
Richard Iribarren <richardiribarren@gmail.com>
2023-03-28 01:45:32 -0400

    build: change modules app to standalone app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Observable, map, shareReplay } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import packageJson from '../../package.json';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule,
+    RouterModule,
+  ],
 })
 export class AppComponent {
-  title = 'firebird-admin';
+  private _breakpointObserver = inject(BreakpointObserver);
+  isHandset$: Observable<boolean> = this._breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+  title = packageJson.name.replace('-', ' ').replace('-', ' ');
+  version = packageJson.version;
 }
