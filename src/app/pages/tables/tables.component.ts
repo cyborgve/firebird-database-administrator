f8f4022109ec03cb492100c2183f5f3cb343affa
Richard Iribarren <richardiribarren@gmail.com>
2023-03-29 16:25:51 -0400

    refactor: update tables page

--- src/app/pages/tables/tables.component.ts
+++ src/app/pages/tables/tables.component.ts
@@ -1,11 +1,14 @@
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tables',
   standalone: true,
-  imports: [CommonModule],
   templateUrl: './tables.component.html',
   styleUrls: ['./tables.component.scss'],
+  imports: [CommonModule, MatButtonToggleModule],
 })
-export class TablesComponent {}
+export class TablesComponent {
+  operations: string[] = ['view', 'create', 'drop', 'edit'];
+}
