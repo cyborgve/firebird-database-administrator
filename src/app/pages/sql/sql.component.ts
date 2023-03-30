1c397aff874d098de847580cbbd6ac4a881d3f88
Richard Iribarren <richardiribarren@gmail.com>
2023-03-30 01:00:34 -0400

    refactor: update sql page

--- src/app/pages/sql/sql.component.ts
+++ src/app/pages/sql/sql.component.ts
@@ -1,10 +1,12 @@
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sql',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule, MatInputModule],
   templateUrl: './sql.component.html',
   styleUrls: ['./sql.component.scss'],
 })
