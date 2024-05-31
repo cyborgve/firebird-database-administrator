cc62a11a7e4db3e408765ee0745e1206be15d5db
Richadr Iribarren <richadriribarren@gmail.com>
2024-05-31 12:44:18 -0400

    build: :fire: update angular builder and adapting rules for vite and eslint

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,5 @@
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { appRoutes } from './app/app-routes';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(BrowserAnimationsModule),
-    provideRouter(appRoutes),
-  ],
-});
+bootstrapApplication(AppComponent, appConfig).catch(err => console.error(err));
